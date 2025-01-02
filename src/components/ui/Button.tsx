c124161fea7125dbdcfa96fb31b66c5791636300
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-01-02 10:33:05 +0530

    Started and almost done with demo lectures

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -8,15 +8,17 @@ const variantStyle = {
 
 const defaultStyles = "flex items-center px-4 py-2 rounded-md transition-all duration-500 hover:-translate-y-2";
 
-export const Button = ({variant , text , startIcon , endIcon}: {
+export const Button = ({variant , text , startIcon , endIcon , onClick}: {
     variant: "primary" | "secondary";
     text: string;
     startIcon?: ReactElement;
-    endIcon?: ReactElement
+    endIcon?: ReactElement;
+    onClick?: () => void;
 }) => {
     return (
         <>
-            <button 
+            <button
+                onClick={onClick}
                 className={`${variantStyle[variant]} ${defaultStyles}`}
             >
                 {startIcon && <div className="mr-2">
--- src/pages/DemoLectures.tsx
+++ src/pages/DemoLectures.tsx
@@ -1,7 +1,12 @@
+import { DemoLectureBox } from "../components/ui/DemoLectureBox"
+
 export const DemoLectures = () => {
     return (
-        <>
-            <div className="text-white">Demolectures</div>
-        </>
+        <div className="flex justify-center flex-wrap">
+            <DemoLectureBox notesPdfUrl="https://drive.google.com/file/d/1XVlMoz-BfXrB_xoTRAE_aOjSEK3-GBKr/view?usp=drive_link" title="Computer Science || python basics , conditional statements , Loops in Python , ForLoop , question practice" videoDriveUrl="https://drive.google.com/file/d/1q6lRU48zNtMYA05deWeiynG4XnP7LSP_/preview" />
+            <DemoLectureBox title="Informatics Practices || Class-11th || Lecture-1: (1st September 2024)
+" videoDriveUrl="https://drive.google.com/file/d/1dTLRRLJu2QH_xy5XIWmg_iX_JZqkwbSn/preview" />
+            <DemoLectureBox title="Informatics Practices || Class-11th || Lecture-2: (7th September 2024)" videoDriveUrl="https://drive.google.com/file/d/1vGHhAwnKZ60TOlDlq3NIiDIs9xsdKdgd/preview" />
+        </div>
     )
 }
