c1ab7276d7ca9cb767de3d94e3652e4d868f3964
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-01-15 18:52:54 +0530

    page redirection if admin not logged in implemented

--- src/courses/AddCourses.tsx
+++ src/courses/AddCourses.tsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export const AddCourses = () => {
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        // Redirect if no token or if the role is not 'admin'
+        if (!token) {
+            navigate("/login");
+            return;
+        }
+    }, [navigate]);
+
     return (
         <div className="text-yellow-200">
-            add the course!! in here
+            Add the course!! in here
         </div>
-    )
+    );
 }
